422bb6887e019ffd9965437a3cf14e5cbe518a22
DeveloperTanmay <umas81261@gmail.com>
2025-02-10 19:06:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+# Python Operators
+
+num1 = 23
+num2 = 13
+
+print("Num1 :", num1)
+print("Num2 :", num2)
+
+print("Addition :" , num1+num2)
+print("Differrence :", num1-num2)
+
+print("Multiplication :", num1*num2)
+
+print("Division :", num1/num2)
+print("Floor Division :" , num1/num2)
+
+
+print("Modulus :", num1%num2)
+
+print("Square :", num1**num2)
+print("Square Root :", num1**0.5)
+print("Square Root :", num2**0.5)
+
+
+print("Equal ? :", num1==num2)
+print("Number 1 greater ? :", num1>num2)
+print("Number 2 greater ? :", num1<num2)
+print("Number 1 is Not Equal To Number 2 ? :", num1!=num2)
+
+result = num1/2*num2+45*num1-32
+print("Result of equation :", result)
+
